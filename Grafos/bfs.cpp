9dcc7433e5e51c60c8070bc36dbc46cf545caaa8
Felipe Costa de Sousa <81444931+felipecostadesousa@users.noreply.github.com>
2024-05-10 09:05:44 -0300

    a

---
+++ Grafos/bfs.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define GRAPH_MAX_SIZE 100
+using namespace std;
+vector<vector<int>> grafo(GRAPH_MAX_SIZE);
+vector<int> distancia(GRAPH_MAX_SIZE,-1);
+vector<int> pai(GRAPH_MAX_SIZE,-1);
+void bfs(int start){
+  vector<bool> visitado(GRAPH_MAX_SIZE,false);
+  queue<int> fila;
+  fila.push(start);
+  stack
+  visitado[start] = true;
+  distancia[start] = 0;
+  while(fila.size()){
+    int u = fila.front();
+    fila.pop();
+    cout << u+1 << endl;
+    for(int w: grafo[u]){
+      if(visitado[w] == false){
+        fila.push(w);
+        visitado[w] = true;
+        distancia[w] = distancia[u] + 1;
+        pai[w] = u;
+      }
+    }
+  }
+}
+int main(){
+  int n,e;
+  cin >> n >> e;
+  for(int i = 0; i < e; i++){
+    int u,v;
+    cin >> u >> v;
+    u--;
+    v--;
+    grafo[u].push_back(v);
+    grafo[v].push_back(u);
+  }
+  bfs(0);
+}
